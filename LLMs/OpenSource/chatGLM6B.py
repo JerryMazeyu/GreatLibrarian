c73bc26b636d9322cdeeee6a430950656ca35aa4
Mimple <2726017426@qq.com>
2023-07-18 21:55:59 +0800

    myz

--- LLMs/OpenSource/chatGLM6B.py
+++ LLMs/OpenSource/chatGLM6B.py
@@ -1,11 +1,41 @@
 from Core import LLMs
+import requests
+import json
+import configparser
+
 
 class ChatGLM6B(LLMs):
     def __init__(self):
         self.model = "ChatGLM-6B"
         pass
-    
+
     def __call__(self, prompt: str) -> str:
-        return "你好，我是ChatGLM6B，很高兴认识你。"
+        # Read the INI file and extract the API address
+        config = configparser.ConfigParser()
+        config.read('../LLMs.ini')  # Replace 'your_ini_file.ini' with the actual file path
+
+        api_address = config.get('chatGLM6B_Local', 'local_api_request_address')
+
+        # Prepare the JSON payload
+        payload = {
+            "prompt": prompt,
+            "history": []
+        }
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        # Make the HTTP POST request with the JSON payload
+        response = requests.post(api_address, headers=headers, data=payload)
+
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            return response.json()['response']
+        else:
+            # Handle request errors
+            print(f"Request failed with status code {response.status_code}: {response.text}")
+            return None
+
 
 chatglm6b = ChatGLM6B()
+# unit test
+# response = chatglm6b.__call__("你好！")
