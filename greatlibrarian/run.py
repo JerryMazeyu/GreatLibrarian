3cf4f2a5a1e00a1be69bfd513540a06743aa0587
EnumaElish123 <1103800163@qq.com>
2024-03-12 14:06:32 +0800

    'Log_path'

--- greatlibrarian/run.py
+++ greatlibrarian/run.py
@@ -8,18 +8,19 @@ import os
 @click.command()
 @click.option(
     "--testcase_path",
-    default=r"E:\GL实验\GL\Test",
+    default="",
     help="testcase的json文件所存放的文件夹路径",
 )
 @click.option(
     "--config_path",
-    default=r"E:\GL实验\GL\register_usr.py",
+    default="",
     help="配置文件的绝对路径",
 )
 @click.option("--project_name", default="", help="项目名称，默认为空字符串")
 @click.option("--test_name", default="", help="实验名称，默认为空字符串")
 @click.option("--test_id", default="", help="实验ID，默认为空字符串")
-def main(testcase_path, config_path, project_name, test_id, test_name) -> None:
+@click.option("--logs_path", default="", help="实验ID，默认为空字符串")
+def main(testcase_path, config_path, project_name, test_id, test_name, logs_path) -> None:
     spec = importlib.util.spec_from_file_location("conf", config_path)
     conf_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(conf_module)
@@ -27,7 +28,7 @@ def main(testcase_path, config_path, project_name, test_id, test_name) -> None:
     Regis = register(config)
     testcase = testcase_path
     if Regis.checkllm():
-        runner = AutoRunner(config, testcase, project_name, test_id, test_name)
+        runner = AutoRunner(config, testcase, project_name, test_id, test_name, logs_path)
         runner.run()
     else:
         error_message = "The __call__ function of your LLM can't work properly!"
@@ -37,7 +38,7 @@ def main(testcase_path, config_path, project_name, test_id, test_name) -> None:
 @click.command()
 @click.option(
     "--config_path",
-    default=r"E:\GL实验\GL\register_usr.py",
+    default="",
     help="配置文件的绝对路径",
 )
 @click.option("--test_id", default="", help="实验ID，默认为空字符串")
